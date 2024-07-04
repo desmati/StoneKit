261cd7a9ea873ba936abd0e749ff7212b2723ece
Hossein Esmati <desmati@gmail.com>
2024-07-04 17:13:53 +0200

    Removed generic type and made the DataStore to detect type by itself

--- Storage/StoneKit.DataStore/DataStoreExtensions.cs
+++ Storage/StoneKit.DataStore/DataStoreExtensions.cs
@@ -12,11 +12,10 @@ public static class DataStoreExtensions
     /// <summary>
     /// Adds the data store to the service collection with the specified options.
     /// </summary>
-    /// <typeparam name="T">The type of objects to store.</typeparam>
     /// <param name="services">The service collection.</param>
     /// <param name="configureOptions">The action to configure the data store options.</param>
     /// <returns>The updated service collection.</returns>
-    public static IServiceCollection AddDataStore<T>(this IServiceCollection services, Action<DataStoreOptions> configureOptions) where T : class
+    public static IServiceCollection AddDataStore(this IServiceCollection services, Action<DataStoreOptions> configureOptions)
     {
         var options = new DataStoreOptions();
         configureOptions(options);
